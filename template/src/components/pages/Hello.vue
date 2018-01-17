935380c9ae24c1e663bd8c0337b23ff8afeb5f93
Jean BARRIERE <jean.barriere@epitech.eu>
2018-01-17 15:53:13 +0700

    feat(app): add vuetify default components

--- template/src/components/pages/Hello.vue
+++ template/src/components/pages/Hello.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="hello">
-    <h1>\{{ $t('alveoli.name') }}</h1>
-    <h2>\{{ $t('alveoli.slogan') }}</h2>
-  </div>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+        <img src="../../assets/v.png" alt="Vue" class="mb-5">
+        <blockquote>
+          &#8220;{{ $('alveoli.slogan') }}&#8221;
+          <footer>
+            <small>
+              <em>&mdash;Unknown</em>
+            </small>
+          </footer>
+        </blockquote>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
 </template>
 
 <script>
@@ -10,10 +21,3 @@ export default {
   name: 'hello'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
-h1, h2 {
-  font-weight: normal;
-}
-</style>
